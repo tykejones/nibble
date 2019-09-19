ba0fc033842257cef06eaa62fcd3c11bdce9fe7f
Tyke Jones <tykejones@gmail.com>
2019-09-19 12:22:34 -0700

    nibble 2nd Commit V2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,13 +4,26 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'Little Star Pizza',
+  address: '1010 Divisadero',
+  city: 'San Francisco',
+  state: 'CA',
+  zipCode: '94117',
+  category: 'Deep Dish',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business];
 class App extends React.Component {
     render(){
     return(
         <div className="App">
   <h1>nibble</h1>
   <SearchBar />
-  <BusinessList /> 
+  <BusinessList businesses={businesses} /> 
 </div>
     );
 }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
+
 class BusinessList extends React.Component {
-    render() {
-        return (
-            <div className = "BusinessList" >
-            <Business /> 
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            <Business />
-            < /div>
-               )
+ render() {
+  return ( 
+   <div className = "BusinessList" > 
+        {
+            this.props.businesses.map((business) => {
+      return <Business business={business} />;
+  })
+        } 
+   </div>
+  );
     }
 }
-
-export default Business;
+export default BusinessList;
